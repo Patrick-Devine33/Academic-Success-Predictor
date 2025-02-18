3b127e66f717e24103a32bfc384788092c6256a5
Maddy Buitenhuis <77648909+mlbuiten515@users.noreply.github.com>
2025-02-18 16:09:46 +0000

    ensuring proper column format based on data description

--- data/data-cleaning.py
+++ data/data-cleaning.py
@@ -1,18 +1,38 @@
+import numpy as np
 import pandas as pd
 
 
 # Read in data set
 df = pd.read_csv('data/assign1-grades.csv')
 
-# Assign valid parameter ranges to each column
+# Assign valid parameter ranges based on csv description to each column
 valid_school = ['GP', 'MS']
+valid_sex = ['F', 'M']
+valid_address = ['U', 'R']
+valid_famsize = ['GT3', 'LT3']
+valid_pstatus = ['T', 'A']
+valid_reason = ['home', 'reputation', 'course', 'other']
+valid_yes_no = ['yes', 'no']
+valid_4scale = np.arange(5)
+valid_5scale = np.arange(6)
+valid_grade = np.arange(21)
 
+minpass_grade = 12
 
-# Ensure columns are properly formatted 
-df['school'] = df['school'].str.upper()
+# Ensure columns are properly formatted
+
+upper_columns = ['school', 'sex', 'address', 'famsize', 'Pstatus']
+lower_columns = ['reason', 'guardian', 'schoolsup', 'famsup', 'paid', 'activities', 'nursery', 'higher', 'internet', 'romantic']
+
+for col in upper_columns:
+    df[col] = df[col].str.upper()
+
+for col in lower_columns:
+    df[col] = df[col].str.lower()
+
 
 # Remove null and invalid data
-df = df[~df['school'].isin(valid_school)]
+df = df[df['school'].isin(valid_school)]
 
 # Show dataset
 print(df.head())
