58e026d95612fce57254db486bd1c2af10b33d74
Maddy Buitenhuis <77648909+mlbuiten515@users.noreply.github.com>
2025-02-28 14:10:58 +0000

    changed dropped columns

--- models/run_svm.py
+++ models/run_svm.py
@@ -2,11 +2,15 @@ import numpy as np
 import pandas as pd
 from sklearn import svm
 from sklearn.model_selection import KFold, GridSearchCV, cross_val_score
+from sklearn.metrics import accuracy_score
 
 # Read in and seperate data
 prep_data = pd.read_csv('data/df_vif_cleaned.csv')
 y = prep_data['result_pass'].astype(int)
-X = prep_data.drop('result_pass', axis=1)
+X = prep_data.drop(['result_pass', 'goout', 'paid', 'Pstatus_T',
+                    'famsize_LE3', 'sex_M', 'romantic', 'traveltime',
+                    'health', 'absences', 'schoolsup', 'famsup', 'activities',
+                    'nursery', 'Dalc', 'Mjob_other', 'Fjob_other'], axis=1)
 X.columns = X.columns.astype(str)
 
 # Define our classifier
@@ -16,20 +20,16 @@ classifier = svm.SVC()
 param_grid = {
     'C': [0.1, 1, 10],
     'kernel': ['linear', 'poly', 'rbf'],
-    'degree': [3, 6, 9],
+    # 'degree': [3, 6, 9],
     'gamma': ['scale', 'auto']
 }
 
-# Set up KFold
+# Set up KFold and grid search
 kfold = KFold(5, shuffle=True, random_state=42)
-for i, (train_index, test_index) in enumerate(kfold.split(X, y)):
-    print(f"Fold {i}:")
-    print(f"  Train: index={train_index}")
-    print(f"  Test:  index={test_index}")
-
 
 grid_search = GridSearchCV(classifier, param_grid, cv=kfold, scoring='accuracy', n_jobs=-1)
 
+print('Starting grid search to optimise hyperparams')
 grid_search.fit(X, y)
 
 best_params = grid_search.best_params_
@@ -44,3 +44,20 @@ average_acc = np.mean(scores)
 
 print(f"Accuracy Score for each fold: {[round(score, 4) for score in scores]}")
 print(f"Average accuracy across 5 folds: {average_acc:.2f}")
+
+for i, (train_index, test_index) in enumerate(kfold.split(X, y)):
+    print(f'fold {i}')
+    X_test, y_test = X.iloc[test_index], y.iloc[test_index]
+    X_train, y_train = X.iloc[train_index], y.iloc[train_index]
+
+    best_svm.fit(X_train, y_train)
+    y_test = np.asarray(y_test)
+    y_pred = best_svm.predict(X_test)
+    misclass = np.where(y_test != y_pred)
+    accuracy = accuracy_score(y_test, y_pred)
+    print(accuracy)
+    print(X_test.iloc[misclass])
+    print(y_test[misclass])
+
+
+# studytime, failures, internet, Walc?, address?, guardian
